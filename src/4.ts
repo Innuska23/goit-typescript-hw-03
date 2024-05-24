527afdf3770a9bb3144293c68a3d4b96c1a9a434
Inna Dmytrenko <inna.shalun@gmail.com>
2024-05-24 20:43:44 +0300

    finished 4 task

--- src/4.ts
+++ src/4.ts
@@ -1,11 +1,53 @@
-// const key = new Key();
+class Key {
+    private signature: number = Math.random();
+    getSignature(): number { return this.signature; }
+}
 
-// const house = new MyHouse(key);
-// const person = new Person(key);
+class Person {
+    constructor(private key: Key) { }
+    getKey(): Key { return this.key }
+}
 
-// house.openDoor(person.getKey());
+abstract class House {
+    protected door: boolean;
+    protected key: Key;
+    protected tenants: Person[] = [];
 
-// house.comeIn(person);
+    constructor(key: Key) {
+        this.door = false;
+        this.key = key;
+    }
+
+    comeIn(person: Person): void {
+        if (this.door) {
+            this.tenants.push(person);
+            console.log('Door open');
+        } else {
+            console.log('Door closed');
+        }
+    }
+    abstract openDoor(key: Key): void;
+}
+
+class MyHouse extends House {
+    openDoor(key: Key): void {
+        if (key.getSignature() === this.key.getSignature()) {
+            this.door = true;
+            console.log('Door open');
+        } else {
+            console.log('Door closed');
+        }
+    }
+}
+
+const key = new Key();
+
+const house = new MyHouse(key);
+const person = new Person(key);
+
+house.openDoor(person.getKey());
+
+house.comeIn(person);
 
 
-// export {};
+export { };
